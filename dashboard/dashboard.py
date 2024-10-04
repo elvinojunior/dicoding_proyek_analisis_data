482df898def84499087384f04906fd8221196ab1
elvino <85337769+elvinojunior@users.noreply.github.com>
2024-10-04 18:20:51 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 from datetime import date
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
